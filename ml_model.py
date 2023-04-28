c745e617b70e68d31c399cf15d3567f9e34d454c
VictoriaAML <vbaretto@zuora.com>
2023-04-28 20:45:40 +0530

    modified the functions in ml model file

--- ml_model.py
+++ ml_model.py
@@ -3,14 +3,14 @@ print("Adding all code related to model")
 def model1():
     print("adding code for ML model 1")
 
-def model1():
-    print("adding code for ML model 1")
-
 def model2():
-    print("adding code for ML model 1")
+    print("adding code for ML model 2")
 
 def model3():
-    print("adding code for ML model 1")
+    print("adding code for ML model 3")
+
+def model4():
+    print("adding code for ML model 4")
 
 
 print("model 2 is best")
